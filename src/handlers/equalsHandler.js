7e20c02424a35782dc1d05b1bc893e32d6eefb7e
nikitagts <nikitasnizko112@gmail.com>
2024-12-23 16:11:30 +0100

    infinity multiplied by zero/ styles

--- src/handlers/equalsHandler.js
+++ src/handlers/equalsHandler.js
@@ -4,6 +4,10 @@ import { updateDisplay } from "../utils/updateDisplay.js";
 export const handleEquals = (state, display) => {
     if (state.prevVal !== null && state.operator !== null) {
         state.currVal = calculate(state.prevVal, state.currVal, state.operator);
+
+        if (isNaN(state.currVal)) {
+            state.currVal = "0";
+        }
         state.prevVal = null;
         state.operator = null;
     }
